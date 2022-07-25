1f3b8c2d0dea4c2f3589e0fdcf81dcaab70fd32c
imxiongstu <0@zejang.cn>
2022-07-25 23:51:39 +0800

    Faui3的RPA自动化

---
+++ XstuDotNetLearning/FlaUI-Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using FlaUI.Core;
+using FlaUI.UIA3;
+using System.Diagnostics;
+
+var processes = Process.GetProcessesByName("WeChat");
+
+using (var app = Application.Attach(processes.First().Id))
+{
+    using (var automation = new UIA3Automation())
+    {
+        var mainWindow = app.GetMainWindow(automation);
+    }
+}
